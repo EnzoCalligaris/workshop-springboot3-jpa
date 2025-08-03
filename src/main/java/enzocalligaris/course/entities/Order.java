f8eb375bb9f6d6400ff57368c89bbda0b4f9fc55
EnzoCalligaris <enzocontatobr@gmail.com>
2025-08-03 19:51:43 -0300

    OrderStatus enum

--- src/main/java/enzocalligaris/course/entities/Order.java
+++ src/main/java/enzocalligaris/course/entities/Order.java
@@ -3,6 +3,7 @@ package enzocalligaris.course.entities;
 
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import enzocalligaris.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -20,6 +21,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -27,10 +30,11 @@ public class Order implements Serializable {
     public Order(){
     }
 
-    public Order(Long id, Instant moment, User clinet) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
-        this.client = clinet;
+        setOrderStatus(orderStatus);
+        this.client = client;
     }
 
     public Long getId() {
@@ -55,6 +59,16 @@ public class Order implements Serializable {
 
     public void setClinet(User clinet) {
         this.client = clinet;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
