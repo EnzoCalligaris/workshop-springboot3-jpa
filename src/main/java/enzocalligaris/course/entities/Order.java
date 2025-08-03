59f8351d7a1006b2332f467d93fcf4bcf86b4971
EnzoCalligaris <enzocontatobr@gmail.com>
2025-08-03 16:49:56 -0300

    Order, Instant, ISO 8601 - PART 1

---
+++ src/main/java/enzocalligaris/course/entities/Order.java
@@ -0,0 +1,68 @@
+package enzocalligaris.course.entities;
+
+
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    public Order(){
+    }
+
+    public Order(Long id, Instant moment, User clinet) {
+        this.id = id;
+        this.moment = moment;
+        this.client = clinet;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public User getClinet() {
+        return client;
+    }
+
+    public void setClinet(User clinet) {
+        this.client = clinet;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
