41a8f9ba00caf646ef195601e63bdf5a2266630f
EnzoCalligaris <enzocontatobr@gmail.com>
2025-08-06 12:19:56 -0300

    Exception handling - delete

---
+++ src/main/java/enzocalligaris/course/services/exceptions/DataBaseException.java
@@ -0,0 +1,8 @@
+package enzocalligaris.course.services.exceptions;
+
+public class DataBaseException extends RuntimeException{
+
+    public DataBaseException(String msg){
+        super(msg);
+    }
+}
